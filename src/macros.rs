dd041410c084453942ba86fa36f94c7990b0f7dc
Usairim Isani <usairim.isani@outlook.com>
2020-07-29 14:42:57 +0500

    Read Line Macro.

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,19 @@
-use std::io::stdin;
-// A convenient macro to read input as string into a buffer
-#[macro_export]
 macro_rules! scanline {
-    ($x:expr) => {{
-        stdin().read_line(&mut $x).unwrap();
-        $x.trim();
+    ($e:expr) => {{
+        use std::io::Read;
+        use std::io::Write;
+        let mut stdout = std::io::stdout();
+        stdout.write_all($e.as_bytes()).unwrap();
+        stdout.flush().unwrap();
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        s
+    }};
+    () => {{
+        use std;
+        let mut s = String::new();
+        let stdin = std::io::stdin();
+        stdin.read_line(&mut s).unwrap();
+        s
     }};
 }
